ab07c3608cdfef20f08b7c916b98b79a801d9a1f
Dilip Kumar <dilipsgm@gmail.com>
2013-07-11 12:11:26 +0530

    added logger and exceptions for PropertyFileReader class

--- src/main/java/com/imaginea/resumereader/helpers/PropertyFileReader.java
+++ src/main/java/com/imaginea/resumereader/helpers/PropertyFileReader.java
@@ -30,41 +30,87 @@ public class PropertyFileReader {
 			createDefaultPropertyFile();
 		} catch (IOException ie) {
 			LOGGER.log(Level.SEVERE,
-					"IOError occured. while reading property file",
-					ie.getMessage());
+					"IOError occured. while reading property file\n ERROR:{0}",
+					new Object[] { ie.getMessage() });
 			throw new IOException(ie);
 		}
 	}
 
 	private void createDefaultPropertyFile() throws FileNotFoundException,
 			IOException {
-		properties.setProperty(INDEX_DIR, "default");
-		properties.setProperty(FILE_DIR, "default");
-		properties.setProperty(LAST_TIME_STAMP, "default");
-		properties.store(new FileOutputStream(FILE_NAME),
-				"Default property file");
+		properties.setProperty(INDEX_DIR, "");
+		properties.setProperty(FILE_DIR, "");
+		this.setLastTimeStamp(new Date());
+		try {
+			properties.store(new FileOutputStream(FILE_NAME),
+					"Default property file");
+		} catch (IOException ie) {
+			LOGGER.log(Level.SEVERE,
+					"IOError occured. while writing property file\n ERROR:{0}",
+					new Object[] { ie.getMessage() });
+			throw new IOException(ie);
+		}
 	}
 
 	public void setIndexDir(String indexDir) throws FileNotFoundException,
 			IOException {
 		properties.setProperty(INDEX_DIR, indexDir);
-		properties.store(new FileOutputStream(FILE_NAME),
-				"index Directory updated");
+		try {
+			properties.store(new FileOutputStream(FILE_NAME),
+					"index Directory updated");
+		} catch (IOException ie) {
+			LOGGER.log(Level.SEVERE,
+					"IOError occured. while writing property file\n ERROR:{0}",
+					new Object[] { ie.getMessage() });
+			throw new IOException(ie);
+		}
 	}
 
+	/**
+	 * returns index directory path
+	 * 
+	 * @return indexDirPath
+	 * @throws IllegalArgumentException
+	 */
 	public String getIndexDir() {
-		return properties.getProperty(INDEX_DIR);
+		String indexDirPath = properties.getProperty(INDEX_DIR).trim();
+		if (indexDirPath.isEmpty()) {
+			LOGGER.log(Level.SEVERE,
+					"Index Directory Path empty, throwing NullPointerException");
+			throw new IllegalArgumentException("index Directory Path is Empty");
+		}
+		return indexDirPath;
 	}
 
 	public void setFileDir(String fileDir) throws FileNotFoundException,
 			IOException {
 		properties.setProperty(FILE_DIR, fileDir);
-		properties.store(new FileOutputStream(FILE_NAME),
-				"File Directory updated");
+		try {
+			properties.store(new FileOutputStream(FILE_NAME),
+					"File Directory updated");
+		} catch (IOException ie) {
+			LOGGER.log(Level.SEVERE,
+					"IOError occured. while writing property file\n ERROR:{0}",
+					new Object[] { ie.getMessage() });
+			throw new IOException(ie);
+		}
 	}
 
+	/**
+	 * returns file directory path
+	 * 
+	 * @return fileDirPath
+	 * @throws IllegalArgumentException
+	 */
+
 	public String getFileDir() {
-		return properties.getProperty(FILE_DIR);
+		String fileDirPath = properties.getProperty(FILE_DIR).trim();
+		if (fileDirPath.isEmpty()) {
+			LOGGER.log(Level.SEVERE,
+					"File Directory Path empty, throwing NullPointerException");
+			throw new IllegalArgumentException("File Directory Path is Empty");
+		}
+		return fileDirPath;
 	}
 
 	public void setLastTimeStamp(Date currTimeStamp)
@@ -72,19 +118,39 @@ public class PropertyFileReader {
 		SimpleDateFormat dateFormatter = new SimpleDateFormat(TIME_STAMP_FORMAT);
 		String dateString = dateFormatter.format(currTimeStamp);
 		properties.setProperty(LAST_TIME_STAMP, dateString);
-		properties.store(new FileOutputStream(FILE_NAME),
-				"Last TimeStamp updated");
+		try {
+			properties.store(new FileOutputStream(FILE_NAME),
+					"Last TimeStamp updated");
+		} catch (IOException ie) {
+			LOGGER.log(Level.SEVERE,
+					"IOError occured. while writing property file\n ERROR:{0}",
+					new Object[] { ie.getMessage() });
+			throw new IOException(ie);
+		}
 	}
 
+	/**
+	 * returns last time stamp
+	 * 
+	 * @return prevTimeStamp
+	 * @throws ParseException
+	 */
 	public Date getLastTimeStamp() throws ParseException {
 		String dateString = properties.getProperty(LAST_TIME_STAMP);
 		SimpleDateFormat dateFormatter = new SimpleDateFormat(TIME_STAMP_FORMAT);
-		Date prevTimeStamp = dateFormatter.parse(dateString);
+		Date prevTimeStamp = null;
+		try {
+			prevTimeStamp = dateFormatter.parse(dateString);
+		} catch (ParseException pe) {
+			LOGGER.log(Level.SEVERE, "ParseException occured \n ERROR:{0}",
+					new Object[] { pe.getMessage() });
+			throw new ParseException(pe.getMessage(), pe.getErrorOffset());
+		}
 		return prevTimeStamp;
 	}
 
 	public static void main(String[] args) throws IOException, ParseException {
 		PropertyFileReader pr = new PropertyFileReader();
-		pr.getLastTimeStamp();
+		System.out.println(pr.getFileDir());
 	}
 }

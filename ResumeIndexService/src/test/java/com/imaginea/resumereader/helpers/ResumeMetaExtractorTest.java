f73ac72626be37e5f2db3cec6a5a2bec91f7a589
Ashwin Sangem <ashwin.sangem@imaginea.com>
2013-08-13 13:58:20 +0530

    junit

---
+++ ResumeIndexService/src/test/java/com/imaginea/resumereader/helpers/ResumeMetaExtractorTest.java
@@ -0,0 +1,28 @@
+package com.imaginea.resumereader.helpers;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.tika.exception.TikaException;
+import org.junit.Test;
+
+public class ResumeMetaExtractorTest {
+	@Test
+	public void getResumeSummaryTest() throws IOException, TikaException {
+		ResumeMetaExtractor resumeMetaExtractor = new ResumeMetaExtractor();
+		String body = "Apurba has 9+ Years of IT experience with an expertise in software design, development as well as architecture and leading of product development efforts";
+		assertTrue("The similarity index should be 1",
+				body.equals(resumeMetaExtractor.getResumeSummary(body)));
+	}
+	
+	@Test
+	public void extractPersonNameTest() throws FileNotFoundException {
+		ResumeMetaExtractor resumeMetaExtractor = new ResumeMetaExtractor();
+		String body = "Apurba Nath \n Apurba has 9+ Years of IT experience with an expertise in software design, development as well as architecture and leading of product development efforts";
+		String personName = resumeMetaExtractor.extractPersonName(body);
+		assertTrue("The similarity index should be 1",
+				"Apurba Nath".equals(personName));
+	}
+}

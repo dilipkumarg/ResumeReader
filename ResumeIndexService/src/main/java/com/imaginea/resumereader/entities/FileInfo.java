1cbf2357c3a9595a4a56c3e1f159832e5219c16a
Ashwin Sangem <ashwin.sangem@imaginea.com>
2013-08-27 11:58:41 +0530

    FileInfo

--- ResumeIndexService/src/main/java/com/imaginea/resumereader/entities/FileInfo.java
+++ ResumeIndexService/src/main/java/com/imaginea/resumereader/entities/FileInfo.java
@@ -8,29 +8,37 @@ package com.imaginea.resumereader.entities;
  * To change this template use File | Settings | File Templates.
  */
 public class FileInfo {
-    private final String filePath;
-    private final String title;
-    private final String summary;
+	private final String filePath;
+	private final String title;
+	private final String summary;
+	private final String content;
 
-    public FileInfo(String filePath, String title, String summary) {
-        this.filePath = filePath;
-        this.title = title;
-        this.summary = summary;
-    }
+	public FileInfo(String filePath, String title, String summary,
+			String content) {
+		this.filePath = filePath;
+		this.title = title;
+		this.summary = summary;
+		this.content = content;
+	}
 
-    public String getFilePath() {
-        return this.filePath;
-    }
+	public String getFilePath() {
+		return this.filePath;
+	}
 
-    public String getTitle() {
-        return this.title;
-    }
+	public String getTitle() {
+		return this.title;
+	}
 
-    public String getSummary() {
-        return this.summary;
-    }
+	public String getSummary() {
+		return this.summary;
+	}
 
-    public String toString() {
-        return "Title:" + this.title + "\n Summary: " + this.summary + "\n File Path: " + this.filePath;
-    }
+	public String toString() {
+		return "Title:" + this.title + "\n Summary: " + this.summary
+				+ "\n File Path: " + this.filePath + "\n\n";
+	}
+
+	public String getContent() {
+		return this.content;
+	}
 }
